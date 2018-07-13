88e38ee27c0c5389db792716dfeea642c14b1301
Ayano <onaya91119@gmail.com>
2018-07-13 18:34:00 +0900

    delete comment out

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -1,15 +1,10 @@
 <?php
 namespace App\Controller;
 use App\Controller\AppController;
-/**
- *
- */
 class UsersController extends AppController
 {
-
   function index()
   {
-    // $id = $this->request->query['id'];
     $data = $this->Users->find('all');
     $this->set('data',$data);
   }
